c496f11fcfb3bb9785fcd45a66a533fb54d59915
Nastia <vau@ugraweb.ru>
2017-02-05 16:44:11 +0500

    Настроен гитигнор и форма ОС на веб странице

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru-RU">
+<head>
+    <meta charset="utf-8" />
+</head>
+<body>
+<form name="feedback" method="post" action="feedback_acttion.php">
+<input type="text" name="name" value="имя">
+<input type="text" name="email" value="email@email.ru">
+<input type="text" name="text" value="сообщение">
+    <input type="submit" value="Отправить">
+</form>
+</body>
+</html>
